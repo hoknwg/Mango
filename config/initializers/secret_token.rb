1f97b7ebb428959a6bc940e906d47204a5628d8b
vagrant <you@example.com>
2014-04-22 18:04:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mango::Application.config.secret_key_base = 'b73668b23ee96ff4ef6aa429b1c0a9338d7f5ad1ebe7b9ead183c8e84741c0feb3fab026939c3e80094f0ace747c2b8b3726ca266e55c4395075ee68b46d4fe7'
